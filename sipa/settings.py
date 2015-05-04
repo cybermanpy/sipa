3f522941d9c30fc2067b27a07d8134f74451f5a0
Diego <fhersa@gmail.com>
2015-05-04 14:02:51 -0400

    Modificación del archivo settings.

--- sipa/settings.py
+++ sipa/settings.py
@@ -91,4 +91,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, '/static/')
+
+MEDIA_URL = os.path.join(BASE_DIR, '/media/')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
