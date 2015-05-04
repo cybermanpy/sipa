87bf4b32e79a3305b3bae62af2df879a6a712623
Diego Saldivar <fhersa@gmail.com>
2015-05-04 18:52:01 +0000

    Modificacion view del home

--- home/views.py
+++ home/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render, render_to_response
 def index(request):
 	title = "SIPA"
 	txt = "Prueba de texto en un parrafo"
-	return render_to_response('index.html', {'title': title, 'txt': txt}, context_instace=RequestContext(request))
+	return render_to_response('index.html', {'title': title, 'txt': txt}, context_instance=RequestContext(request))
